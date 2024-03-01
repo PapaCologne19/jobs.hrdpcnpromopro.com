e2424690dc05afc6acc7d870732c842185247972
FIRST_NAME LAST_NAME <jphigomera0619@gmail.com>
2024-03-01 14:59:02 +0800

    MARCH 01, 2024

--- database/connection.php
+++ database/connection.php
@@ -1,7 +1,7 @@
 <?php
 $localhost = "localhost";
-$user = "pcnadmin";
-$pass = "@Pcn123456789#";
+$user = "root";
+$pass = "";
 $database = "pcnpromopro_pcnhrs";
 
 $con = mysqli_connect($localhost, $user, $pass, $database);
